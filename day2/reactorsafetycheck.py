7739d144447ebbe84ae6d99156ade3610c34d0a2
vic-cio <vc427@bath.ac.uk>
2024-12-02 12:34:30 +0000

    added day2 python and markdown files

---
+++ day2/reactorsafetycheck.py
@@ -0,0 +1,9 @@
+import os
+
+# directory = "/Users/victorciobanu/Documents/Programming/advent-of-code-2024/day1"
+# os.chdir(directory)
+
+file_path = 'rednosereactorlevels.md'
+
+with open(file_path, 'r') as file:
+    levelsdata = file.read()
