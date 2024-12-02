2ca7008afab1f183f7d72fb3004bfda39952f7fc
vic-cio <vc427@bath.ac.uk>
2024-12-02 19:18:35 +0000

    day 2 complete

--- day2/reactorsafetycheck.py
+++ day2/reactorsafetycheck.py
@@ -15,29 +15,27 @@ def is_safe(levels):
 
 """Checks to see whether a sequence of levels is mostly safe"""
 def is_mostly_safe(levels):
-    if max(is_safe(levels)): # If the levels are fully safe, return True
+    if is_safe(levels): 
         return True
 
     for i in range(len(levels)):
-        modified_levels = levels[:i] + levels[i + 1:]  # Remove the offending element
+        modified_levels = levels[:i] + levels[i+1:] 
         if is_safe(modified_levels):
             return True
         
     return False
 
 
-"""testing ground"""
-test_data = [[7,6,4,2,1], [1,2,7,8,9], [9,7,6,2,1], [1,3,2,4,5], [8,6,4,4,1], [1,3,6,7,9]]
-print(sum([is_mostly_safe(entry) for entry in test_data]))
-print(is_mostly_safe([7,6,4,2,1]))
+# """testing ground"""
+# test_data = [[7,6,4,2,1], [1,2,7,8,9], [9,7,6,2,1], [1,3,2,4,5], [8,6,4,4,1], [1,3,6,7,9]]
+# print(sum([is_mostly_safe(entry) for entry in test_data]))
+# print(is_mostly_safe([7,6,4,2,1]))
 
 
-
-# safe_reports = sum([is_safe(list(map(int, entry.split(" ")))) for entry in levels_list])
-# mostly_safe_reports = sum([is_mostly_safe(list(map(int, entry.split(" ")))) for entry in levels_list])
-
-# print(safe_reports)
-# print(mostly_safe_reports)
+safe_reports = sum([is_safe(list(map(int, entry.split(" ")))) for entry in levels_list])
+mostly_safe_reports = sum([is_mostly_safe(list(map(int, entry.split(" ")))) for entry in levels_list])
+print(safe_reports)
+print(mostly_safe_reports)
 
 
 # """old clumsy way of reading file"""
