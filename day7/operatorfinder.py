bd56d4ba226e0d66c415e8cd82cf59e3f978ee1e
vic-cio <vc427@bath.ac.uk>
2024-12-08 16:32:38 +0000

    day 6 start

---
+++ day7/operatorfinder.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+current_directory = Path(__file__).parent # Get the directory of the running python script
+file_path = current_directory / 'calibrationvalues.txt' # Join the directory with the file name
+calibration_str = file_path.read_text()
+
+print(calibration_str)
