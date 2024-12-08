14cf3b27009f37a6dcff6dcc247df33a95e20fc0
vic-cio <vc427@bath.ac.uk>
2024-12-08 14:24:08 +0000

    made part 1 modular to help with part 2

--- day6/guardpositions.py
+++ day6/guardpositions.py
@@ -3,80 +3,96 @@ import numpy as np
 
 current_directory = Path(__file__).parent
 grid_string = (current_directory / 'gridwithobstacles.txt').read_text()
+lab_grid = [[char for char in row] for row in grid_string.split('\n')]
 
-grid = [[char for char in row] for row in grid_string.split('\n')]
+"""Find the guard's position and orientation"""
+
+def findguard(grid):  # Get the guard's position and orientation
+    for y, row in enumerate(grid):
+        for x, cell in enumerate(row):
+            if cell in "^v<>":
+                print ("Guard found:", x, y, cell)
+                return (x, y, cell)
+
+"""takes coords and grid parameters"""
+def inbound(x, y, length, height):  
+    print("Inbound? (", x , y, ")", "(", length, height, ")")
+    return 0 <= x < length and 0 <= y < height
+
+def next_pos_and_direction(x, y, direction):
+    if direction == "^":
+        return (x, y - 1, direction)
+    elif direction == ">":
+        return (x + 1, y, direction)
+    elif direction == "v":
+        return (x, y + 1, direction)
+    elif direction == "<":
+        return (x - 1, y, direction)
 
 def path_finder(grid):
-    """Find the guard's position and orientation"""
-    def findguard(grid):  # Get the guard's position and orientation
-        for y, row in enumerate(grid):
-            for x, cell in enumerate(row):
-                if cell in "^v<>":
-                    return (x, y, cell)
-        return None  # Guard not found
+    x_len = len(grid[0])
+    y_len = len(grid)
 
-    def inbound(coords):  # Coords aren't outside the grid
-        x, y = coords
-        return 0 <= x < len(grid[0]) and 0 <= y < len(grid)
-
-    def next_pos_and_direction(current_state):
-        x, y, direction = current_state
-        if direction == "^":
-            return (x, y - 1, direction)
-        elif direction == ">":
-            return (x + 1, y, direction)
-        elif direction == "v":
-            return (x, y + 1, direction)
-        elif direction == "<":
-            return (x - 1, y, direction)
-
+    active_grid = [list(row) for row in grid] # Grid to be dynamically updated
     orientations = "^>v<"  # Order of rotations for directions
 
-    # Mutable representation of the grid
-    active_grid = [list(row) for row in grid]
     initial_state = findguard(active_grid)
     if initial_state is None:
         return "Guard not found"
-
+    
     current_state = initial_state
+    active_grid[current_state[1]][current_state[0]] = "X"
+
     distinct_points = {current_state[:2]}
-    active_grid[current_state[1]][current_state[0]] = "X"  # Mark visited
+    print(distinct_points)
+    distinct_states = {current_state}
+    print(distinct_states)
+    print(len(distinct_points))
 
-    while True:
-        next_state = next_pos_and_direction(current_state)
+    # distinct_states.add((4, 7, "^"))
+    # if (4, 7, "^") in distinct_states:
+    #     return 0
 
-        if not inbound(next_state[:2]):
-            return len(distinct_points)
+    moves = 0
+    while moves < 15000:
+        print("Move:", moves)
+        next_state = next_pos_and_direction(current_state[0], current_state[1], current_state[2])
+        print("trying next state:", next_state)
 
-        next_x, next_y, next_direction = next_state
-        next_cell = active_grid[next_y][next_x]
+        if inbound(next_state[0], next_state[1], x_len, y_len):
+            next_cell = active_grid[next_state[1]][next_state[0]]
+            print("next cell:", next_cell)
+        else:
+            print("Out of bounds")
+            return len(distinct_points), distinct_points
+        
+        if next_state in distinct_states:
+            print("loop detected")
+            return 0, set()
+        
+        elif next_cell == "#":
+            print("Obstacle detected")
+            next_state = (current_state[0], current_state[1],
+                             orientations[(orientations.index(current_state[2]) + 1) % len(orientations)])
+    
+        elif next_cell == "X":
+            print("Visited cell")
 
-        if next_cell == ".":  # Move forward
-            distinct_points.add((next_x, next_y))
-            active_grid[next_y][next_x] = "X"
-            current_state = next_state
+        elif next_cell == ".":
+            active_grid[next_state[1]][next_state[0]] = "X"
+            print("New cell")
 
-        elif next_cell == "X":  # Continue moving forward
-            current_state = next_state
+        else:
+            return ("case not found"), distinct_points
+        
+        current_state = next_state
+        distinct_points.add(current_state[:2])
+        distinct_states.add(current_state)
 
-        elif next_cell == "#":  # Turn right
-            current_direction = current_state[2]
-            new_direction = orientations[(orientations.index(current_direction) + 1) % len(orientations)]
-            current_state = (current_state[0], current_state[1], new_direction)
+        moves += 1
 
-    return "Move limit exceeded"
+    return ("out of moves"), distinct_points
 
-
-# print(path_finder(grid))
-
-# """So ugly :("""
-# print(sum([
-#     sum(
-#         path_finder(grid, (x, y)) 
-#         for x in range(len(grid[0]))
-#     ) 
-#     for y in range(len(grid))
-# ]))
 
 
 
@@ -93,15 +109,15 @@ test_grid_string = """....#.....
 #.........
 ......#..."""
 
-test_grid = [[char for char in row] for row in test_grid_string.split('\n')]
+test_2 = """.#..
+.^.#
+#...
+..#."""
+
+test_grid = [[char for char in row] for row in test_grid_string.split('\n')] #find path
+test_grid_2 = [[char for char in row] for row in test_2.split('\n')] #find loop
 
 print(path_finder(test_grid))
+print(path_finder(test_grid_2))
+print(path_finder(lab_grid)) #find path
 
-# """So ugly :("""
-# print(sum([
-#     sum(
-#         path_finder(test_grid, (x, y)) 
-#         for x in range(len(test_grid[0]))
-#     ) 
-#     for y in range(len(test_grid))
-# ]))
